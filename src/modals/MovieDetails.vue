caddeff21fd97458f996d82595539d9e7120324b
arielrahmane <arielrahmane@gmail.com>
2021-06-29 23:12:39 +1000

    movie detail modal working

---
+++ src/modals/MovieDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-title>{{ movie.Name }}</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="ion-padding">
+    
+    <ion-img :src="movie.SkinMobileUrl" alt="No Available Image"></ion-img>
+  </ion-content>
+</template>
+
+<script>
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg,  } from '@ionic/vue';
+import { defineComponent  } from 'vue';
+
+export default defineComponent ({
+  name: 'MovieDetailsModal',
+  props: {
+    movie: {},
+  },
+  components: { 
+      IonContent, 
+      IonHeader, 
+      IonTitle, 
+      IonToolbar,
+      IonImg,
+    }
+});
+
+</script>
