edf070a56a6ad84626b07497f7d72f716934ac5c
arielrahmane <arielrahmane@gmail.com>
2022-04-24 17:04:03 +1000

    single view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,34 +1,16 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import Tabs from '../views/Tabs.vue';
+import MoviesTab from '../views/MoviesTab.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/ionicComp'
+    redirect: '/movies'
   },
   {
-    path: '/tabs/',
-    component: Tabs,
-    children: [
-      {
-        path: '',
-        redirect: '/tabs/ionicComp'
-      },
-      {
-        path: 'ionicComp',
-        component: () => import('@/views/IonicComp.vue')
-      },
-      {
-        path: 'movies',
-        component: () => import('@/views/MoviesTab.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3.vue')
-      }
-    ]
-  }
+    path: '/movies',
+    component: MoviesTab,
+  },
 ]
 
 const router = createRouter({
